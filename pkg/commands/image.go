128320959ae46ec64c0a8016401862e057afdc68
RAJA PREM SAI MADDULA <123068364+RajaPremSai@users.noreply.github.com>
2025-11-11 21:22:49 +0530

    Image Command

--- pkg/commands/image.go
+++ pkg/commands/image.go
@@ -1,1 +1,22 @@
 package commands
+
+import (
+	"github.com/RajaPremSai/go-openai-dicord-bot/pkg/bot"
+	"github.com/RajaPremSai/go-openai-dicord-bot/pkg/commands/dalle"
+	discord "github.com/bwmarrin/discordgo"
+	"github.com/sashabaranov/go-openai"
+)
+
+const imageCommandName = "image"
+
+func ImageCommand(client *openai.Client) *bot.Command {
+	return &bot.Command{
+		Name:                     imageCommandName,
+		Description:              "Generate creative images from textual description",
+		DMPermission:             false,
+		DefaultMemberPermissions: discord.PermissionViewChannel,
+		SubCommands: bot.NewRouter([]*bot.Command{
+			dalle.Command(client),
+		}),
+	}
+}
